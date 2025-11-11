09ffd787cf852db01ff9153a28530488468da8c6
Md Samim <mdsamim205008@gmail.com>
2025-11-11 15:23:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3768348144-8e476",
+  "appId": "1:89861169631:web:02afc408e5928a5b3ce46a",
+  "apiKey": "AIzaSyDcsBbfvPj6dNZTFSAnUYx4u_fd5h-nZoE",
+  "authDomain": "studio-3768348144-8e476.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "89861169631"
+};
