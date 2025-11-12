18e07206938d2a10fb2a3362cba20466b080af02
Md Samim <mdsamim205008@gmail.com>
2025-11-12 08:59:45 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.drz.lazcdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
