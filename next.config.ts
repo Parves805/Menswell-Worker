9c8f8a85a64b6d272ac0bb2a78bd3782edf3ea91
Md Samim <mdsamim205008@gmail.com>
2025-11-12 09:40:22 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'khelacricket.blogspot.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
