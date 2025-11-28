a50dc1eae1883501fc78f148602ae41a6e3d4083
Md Samim <mdsamim205008@gmail.com>
2025-11-28 16:34:43 +0000

    const firebaseConfig = {   apiKey: "AIzaSyA0V84Lnb5F9-tFTmEsHmelmc5YQS-o

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "studio-3768348144-8e476",
-  "appId": "1:89861169631:web:02afc408e5928a5b3ce46a",
-  "storageBucket": "studio-3768348144-8e476.appspot.com",
-  "apiKey": "AIzaSyDcsBbfvPj6dNZTFSAnUYx4u_fd5h-nZoE",
-  "authDomain": "studio-3768348144-8e476.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "89861169631"
+  apiKey: "AIzaSyA0V84Lnb5F9-tFTmEsHmelmc5YQS-o8ng",
+  authDomain: "flexfusion-8b64c.firebaseapp.com",
+  databaseURL: "https://flexfusion-8b64c-default-rtdb.firebaseio.com",
+  projectId: "flexfusion-8b64c",
+  storageBucket: "flexfusion-8b64c.appspot.com",
+  messagingSenderId: "413335501485",
+  appId: "1:413335501485:web:b806ff062854f2a5e96424",
+  measurementId: "G-XDNJF65583"
 };
